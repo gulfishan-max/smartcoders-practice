7b0103d2e698af8234d03eb1aab2d4d03b670089
gulfishan-max <mdgulfishan186@gmail.com>
2026-09-08 16:06:13 +0530

    Add files via upload

---
+++ #greater two numbers.py
@@ -0,0 +1,7 @@
+#greater two numbers
+a=int(input("enter the  first number:"))
+b=int(input("enter the secound number:"))
+if a>b:
+    print(a)
+else:
+    print(b)    
---
+++ #positive ;negative and zero.py
@@ -0,0 +1,9 @@
+#positive ;negative and zero
+a=int(input("enter the number :"))
+b=int(input("enter the number :"))
+if a>b:
+    print("the number is positive")
+elif a<b:
+    print("the number is negative") 
+else:
+    print("zero")       
